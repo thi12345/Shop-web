61c149e6c78665f445102c6de5f183d5718d3f67
thi12345 <thinguyen130304@gmail.com>
2025-04-04 19:45:21 +0700

    first commit for error handle

---
+++ Backend/Backend/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+            if(thing == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+            var thingToReturn = thing.ToString();
+            return Ok();
+        }
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
--- Backend/Backend/Controllers/ProductsController.cs
+++ Backend/Backend/Controllers/ProductsController.cs
@@ -11,7 +11,7 @@ namespace Backend.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
